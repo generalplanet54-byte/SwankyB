e97d995ac44ca99524a0c1a98474e034ff26db22
generalplanet54-byte <general.planet.54@gmail.com>
2025-10-05 23:16:00 +0000

    new file:   src/components/EnvBanner.tsx

---
+++ src/components/EnvBanner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EnvBanner: React.FC = () => {
+  return (
+    <div style={{ background: '#eee', color: '#333', padding: '8px', textAlign: 'center' }}>
+      EnvBanner Placeholder
+    </div>
+  );
+};
+
+export default EnvBanner;
