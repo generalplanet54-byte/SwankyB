2efd52b1d297013023bb89e83272ae5660bc4c2f
generalplanet54-byte <general.planet.54@gmail.com>
2025-10-25 06:17:46 +0000

    fix: Update product images with proper Amazon URLs

---
+++ migrations/d1/007_fix_product_images.sql
@@ -0,0 +1,73 @@
+-- Update old products with proper image URLs from the seed data
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71WYNsKWu2L._AC_SL1500_.jpg'
+WHERE id = 1; -- Braun Series 9 Pro
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71DkfT+iakL._AC_SL1500_.jpg'
+WHERE id = 2; -- Philips Norelco 9000
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71L-tKLN1VL._AC_SL1500_.jpg'
+WHERE id = 3; -- Panasonic Arc5
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71j3eRBaknL._AC_SL1500_.jpg'
+WHERE id = 4; -- Braun Series 7
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71SLqWKnRpL._AC_SL1500_.jpg'
+WHERE id = 5; -- Remington F5-5800
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/715e2z8x0EL._AC_SL1500_.jpg'
+WHERE id = 6; -- Philips Norelco Multigroom 7000
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71lA5g426jL._AC_SL1500_.jpg'
+WHERE id = 7; -- Wahl Clipper
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71B-LL9+B-L._AC_SL1500_.jpg'
+WHERE id = 8; -- Brio Beardscape Pro
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71c6-g5ORhL._AC_SL1500_.jpg'
+WHERE id = 9; -- Philips Bodygroom 7000
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71j5t+5kL-L._AC_SL1500_.jpg'
+WHERE id = 10; -- Braun Body Groomer 5
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71vAFOa229L._AC_SL1500_.jpg'
+WHERE id = 11; -- MANSCAPED Lawn Mower 4.0
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71q9d7N2S+L._AC_UX679_.jpg'
+WHERE id = 12; -- Everlane Organic Cotton Crew
+
+UPDATE products SET 
+  primary_image = 'https://www.jcrew.com/s7-img-facade/BE799_WT0002_m?wid=416'
+WHERE id = 13; -- J.Crew Broken-in T-shirt
+
+UPDATE products SET 
+  primary_image = 'https://image.uniqlo.com/UQ/ST3/us/imagesgoods/455359/item/usgoods_00_455359.jpg'
+WHERE id = 14; -- Uniqlo U Crew Neck
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71bA8P-j5ZL._AC_SL1500_.jpg'
+WHERE id = 15; -- Bowflex SelectTech 552
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71sYJg+Jd-L._AC_SL1500_.jpg'
+WHERE id = 16; -- TRX Suspension Trainer
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/61vJtKbAssL._AC_SL1500_.jpg'
+WHERE id = 17; -- Sony WH-1000XM5
+
+UPDATE products SET 
+  primary_image = 'https://m.media-amazon.com/images/I/71+2V6xj+PL._AC_SL1500_.jpg'
+WHERE id = 18; -- Bose QuietComfort Ultra
